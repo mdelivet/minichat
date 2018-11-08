e231458642ac2fd3ea3e80026387143f56a48ace
mdelivet <mdelivet@gmail.com>
2018-11-08 13:42:19 +0100

    chang

--- minichat.php
+++ minichat.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<?php
+
+session_start();
+?>
 <html>
     <head>
         <meta charset="utf-8" />
@@ -14,14 +18,27 @@
     
     <form action="minichat_post.php" method="post">
         <p>
-        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+         <?php
+            if(isset($_SESSION['pseudoID'] )){
+                echo'<label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />';
+                echo'<input type="submit" value="Envoyer" /><br />';
 
-        <input type="submit" value="Envoyer" />
-  </p>
+                echo'<a href="deconnect.php">Se deconnecter</a>';
+            }else{
+                echo' <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />'; 
+                echo '<label for="pass">Mot de passe</label> : <input type="password" name="pass" id="pass" /><br />';
+                echo'<input type="submit" value="Envoyer" />';
+
+            }
+        ?>
+
+
+            
+        </p>
     </form>
 
 <?php
+
 try
 {
   $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
@@ -31,7 +48,7 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+$reponse = $bdd->query('SELECT utilisateurs.id,utilisateurs.pseudo, minichat.message, minichat.idpseudo FROM minichat, utilisateurs Where minichat.idpseudo = utilisateurs.id ORDER BY ID DESC LIMIT 0, 10');
 
 while ($donnees = $reponse->fetch())
 {
--- minichat_post.php
+++ minichat_post.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
@@ -8,8 +9,25 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-$req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
-$req->execute(array($_POST['pseudo'], $_POST['message']));
+if(isset($_SESSION['pseudoID'] )){
+$req = $bdd->prepare('INSERT INTO minichat (idpseudo, message) VALUES(?, ?)');
+$req->execute(array($_SESSION['pseudoID'], $_POST['message']));
+}else{
+
+$reponse = $bdd->query('SELECT id, pseudo, motDePasse FROM utilisateurs ORDER BY ID ');
+
+while ($donnees = $reponse->fetch())
+{
+	if($donnees['pseudo'] == $_POST['pseudo'] && $donnees['motDePasse'] == $_POST['pass']){
+		$_SESSION['pseudoID']=$donnees['id'];
+	}
+}
+
+$reponse->closeCursor();
+}
+
+
+
 
 header('Location: minichat.php');
 ?>
